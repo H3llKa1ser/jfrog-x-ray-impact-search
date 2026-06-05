de97fe3fffc16b720f2edb9500968fdd963c17b9
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2026-06-05 13:09:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Windows
-GOOS=windows GOARCH=amd64 go build -o build/x-ray_cve_check_windows_amd64.exe x-ray_cve_check.go
-GOOS=windows GOARCH=386   go build -o build/x-ray_cve_check_windows_386.exe x-ray_cve_check.go
-GOOS=windows GOARCH=arm64 go build -o build/x-ray_cve_check_windows_arm64.exe x-ray_cve_check.go
+GOOS=windows GOARCH=amd64 go build -o build/x-ray_impact_search_windows_amd64.exe x-ray_impact_search.go
+GOOS=windows GOARCH=386   go build -o build/x-ray_impact_search_windows_386.exe x-ray_impact_search.go
+GOOS=windows GOARCH=arm64 go build -o build/x-ray_impact_search_windows_arm64.exe x-ray_impact_search.go
 
 # Linux
-GOOS=linux GOARCH=amd64 go build -o build/x-ray_cve_check_linux_amd64 x-ray_cve_check.go
-GOOS=linux GOARCH=arm64 go build -o build/x-ray_cve_check_linux_arm64 x-ray_cve_check.go
+GOOS=linux GOARCH=amd64 go build -o build/x-ray_impact_search_linux_amd64 x-ray_impact_search.go
+GOOS=linux GOARCH=arm64 go build -o build/x-ray_impact_search_linux_arm64 x-ray_impact_search.go
 
 # macOS
-GOOS=darwin GOARCH=amd64 go build -o build/x-ray_cve_check_macos_amd64 x-ray_cve_check.go
-GOOS=darwin GOARCH=arm64 go build -o build/x-ray_cve_check_macos_arm64 x-ray_cve_check.go
+GOOS=darwin GOARCH=amd64 go build -o build/x-ray_impact_search_macos_amd64 x-ray_impact_search.go
+GOOS=darwin GOARCH=arm64 go build -o build/x-ray_impact_search_macos_arm64 x-ray_impact_search.go
 
 echo "✅ All builds complete in ./build/"
